1faca5e2ca08c0939b1165e273edb8ce1cc47866
Finn Woelm <public@finnwoelm.com>
2021-02-01 21:05:48 +0100

    Pipeline SDGs: Track matching keyword
    Track the keyword that resulted in the match.

--- helpers/find_sdg_keywords_in_text.py
+++ helpers/find_sdg_keywords_in_text.py
@@ -3,23 +3,34 @@ from helpers.get_context import get_context
 
 # Load regex patterns
 regex_patterns = get_sdg_regex_patterns()
+main_pattern = regex_patterns["main"]
+pattern_items = regex_patterns["items"]
 
 
 def find_sdg_keywords_in_text(text, tag):
     matches = []
 
     # Search for keywords in string
-    for goal, pattern in regex_patterns.items():
-        for match in pattern.finditer(text):
-            matches.append(
-                {
-                    "sdg": goal,
-                    "keyword": match.group(),
-                    "context": get_context(
-                        text, start=match.start(), end=match.end(), context=50
-                    ),
-                    "tag": tag,
-                }
-            )
+    for match in main_pattern.finditer(text):
+        matched_string = match.group()
+
+        match_found = False
+        for item in pattern_items:
+            # Add one match for each matching pattern
+            if item["pattern"].match(matched_string):
+                match_found = True
+                matches.append(
+                    {
+                        "sdg": item["sdg"],
+                        "keyword": item["keyword"],
+                        "context": get_context(
+                            text, start=match.start(), end=match.end(), context=50
+                        ),
+                        "tag": tag,
+                    }
+                )
+
+        if not match_found:
+            raise Exception(f"Could not find match for {matched_string}")
 
     return matches
