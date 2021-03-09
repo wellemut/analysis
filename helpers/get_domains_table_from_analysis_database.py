35f61dc36e0a90044ff85e2bd470b8cd748b463d
Finn Woelm <public@finnwoelm.com>
2021-03-09 08:57:42 +0100

    Delete unused helpers and results
    Remove helpers and results that are no longer needed with our new 
    database structure.

--- helpers/get_domains_table_from_analysis_database.py
+++
@@ -1,5 +0,0 @@
-from models.Database import Database
-
-
-def get_domains_table_from_analysis_database():
-    return Database("analysis").table("domains")
--- helpers/get_urls_table_from_scraped_database.py
+++
@@ -1,5 +0,0 @@
-from models.Database import Database
-
-
-def get_urls_table_from_scraped_database():
-    return Database("scraped").table("urls")
