273fb015c83fcda23714fb84cd1d88e27843c24d
Finn Woelm <public@finnwoelm.com>
2022-01-03 18:30:39 +0100

    ExtractPipeline: Add URL blocklist and ignore matching pages
    For some pages, we do not want to extract and analyze the content. For
    example, for privacy policy pages or for news articles. To ignore these
    pages, the ExtractPipeline has a URL_BLOCKLIST that is used to skip a
    URL if any of the URL's path segments are contained in the blocklist.

--- pipelines/ExtractPipeline.py
+++ pipelines/ExtractPipeline.py
@@ -1,9 +1,72 @@
+import re
+from urllib.parse import urlparse
 from sqlalchemy.orm import load_only
 from helpers.extract_texts_from_html import extract_texts_from_html
 from models import Website, Webpage, TextBlock, WebpageTextBlock
 
 
 class ExtractPipeline:
+    FILE_EXTENSION_REGEX = re.compile(r"\.(html|php|htm)$")
+    # URLs that contain one of the following terms are ignored
+    URL_BLOCKLIST = [
+        "privacy",
+        "privacypolicy",
+        "privacypolicyen",
+        "privacypolicy2",
+        "privacynotice",
+        "privacystatement",
+        "privacydatenschutz",
+        "dataprivacystatement",
+        "appprivacy",
+        "dataprivacy",
+        "dataprotection",
+        "dataprotectionstatement",
+        "datasecurity",
+        "imprintprivacy",
+        "datenschutz",
+        "datenschutzhinweis",
+        "datenschutzerklaerung",
+        "datenschutzimpressum",
+        "gdpr",
+        "gpdr",
+        "agb",
+        "agben",
+        "agbs",
+        "allgemeinegeschaeftsbedingungen",
+        "eula",
+        "terms",
+        "termsofuse",
+        "generaltermsandconditions",
+        "termsconditions",
+        "termsandconditions",
+        "termsofservice",
+        "legal",
+        "imprint",
+        "imprintandprivacyprotection",
+        "nutzungsbedingungen",
+        "rss",
+        "feed",
+        "press",
+        "presspage",
+        "pressreleases",
+        "presse",
+        "pressemitteilungen",
+        "archive",
+        "nachrichtenarchiv",
+        "archiv",
+        "blog",
+        "posts",
+        "post",
+        "news",
+        "aktuelles",
+        "aktuellemeldungen",
+        "sitemap",
+        "impressum",
+        "contact",
+        "kontakt",
+        "jobs",
+    ]
+
     @classmethod
     def process(cls, domain):
         # Store new webpage text blocks (associations between webpages and
@@ -23,13 +86,19 @@ class ExtractPipeline:
 
         # For each page, ...
         for id in webpage_ids:
-            # ... extract text blocks from HTML content
-            for text in extract_texts_from_html(Webpage.find(id).content):
+            webpage = Webpage.find(id)
 
-                # ... find or create associated text block
+            # ... skip if the URL is blocklisted (e.g., privacy policy or news)
+            if cls.is_url_blocklisted(webpage.url):
+                continue
+
+            # ... eotherwise, extract text blocks from HTML content
+            for text in extract_texts_from_html(webpage.content):
+
+                # Find or create associated text block
                 block = TextBlock.find_by_content_or_create(content=text["content"])
 
-                # ... and set up text block association
+                # Set up text block association
                 webpage_text_blocks.append(
                     WebpageTextBlock().fill(
                         webpage_id=id, text_block_id=block.id, tag=text["tag"]
@@ -45,3 +114,27 @@ class ExtractPipeline:
             # Save new text block associations
             for webpage_text_block in webpage_text_blocks:
                 webpage_text_block.save()
+
+    @classmethod
+    def is_url_blocklisted(cls, url):
+        # Get URL path
+        path = urlparse(url).path
+
+        # Convert path to lowercase
+        path = path.lower()
+
+        # Remove file extension, if any
+        path = cls.FILE_EXTENSION_REGEX.sub("", path, count=1)
+
+        # Remove trailing slash, if any
+        path = path.rstrip("/")
+
+        # Removing any separators
+        path = path.translate(str.maketrans("", "", "-_"))
+
+        # Check if any path segments match
+        for segment in path.split("/"):
+            if segment in cls.URL_BLOCKLIST:
+                return True
+
+        return False
--- tests/pipelines/extract_test.py
+++ tests/pipelines/extract_test.py
@@ -42,6 +42,15 @@ def test_it_ignores_pages_without_content():
     assert TextBlock.query.count() == 1
 
 
+def test_it_ignores_blocklisted_urls():
+    page = Webpage.create_from_url("https://www.17ziele.de/privacy.html")
+    page.update(content="<body><p>ignore content</p></body>")
+
+    ExtractPipeline.process("17ziele.de")
+
+    assert TextBlock.query.count() == 0
+
+
 def test_it_removes_existing_text_block_associations_for_the_website():
     webpage = Webpage.create_from_url("https://www.17ziele.de")
     webpage.update(content="<body>hello world</body>")
@@ -61,3 +70,89 @@ def test_it_removes_existing_text_block_associations_for_the_website():
 
     # Does not touch other webpage's text blocks
     assert len(other_webpage.reload().webpage_text_blocks) == 1
+
+
+def describe_is_url_blocklisted():
+    privacy_policies = [
+        "https://www.tier.app/privacy.html",
+        "https://www.strayz.de/Privacy",
+        "https://www.tier.app/de/privacy-notice/",
+        "https://www.tier.app/privacy-policy/",
+        "https://pi.pe/privacy_policy/index.html",
+        "https://www.zolar.de/datenschutz",
+        "https://niyok.de/pages/datenschutz-impressum",
+        "https://www.silfir.com/privacy-datenschutz",
+        "https://www.ecf-farmsystems.com/datenschutzerklaerung/",
+        "https://www.solarkiosk.eu/data-protection/",
+        "https://humanoo.com/en/data-security/",
+        "https://www.12tree.de/privacy-statement",
+        "https://cogniscent.io/privacy-policy-en",
+        "https://mitte.co/privacy-policy-2/",
+        "https://smarterials.berlin/en/gpdr/",
+        "https://sanitygroup.com/data-privacy/",
+        "https://www.milkthesun.com/en/data-privacy-statement",
+        "https://www.epigenomics.com/imprint/data-protection-statement/",
+        "https://www.noerr.com/de/meta/datenschutz",
+        "https://www.alnatura.de/de-de/ueber-uns/datenschutzhinweis/",
+    ]
+
+    terms_and_conditions = [
+        "https://www.tier.app/agb.html",
+        "https://www.tier.app/de/terms-and-conditions/",
+        "https://www.ansmann.de/no/general-terms-and-conditions",
+        "http://www.ea.com/terms-of-service",
+        "https://klima.com/terms-of-service/",
+        "https://chatterbug.com/en/legal/terms",
+        "https://greenfashiontours.com/imprint-and-privacy-protection/",
+        "https://marleyspoon.com/terms",
+        "https://www.fixfirst.de/nutzungsbedingungen",
+        "https://plantix.net/imprint",
+        "https://www.lindera.de/agbs",
+        "https://plana.earth/terms-conditions",
+        "https://www.zolar.de/allgemeine-geschaeftsbedingungen",
+        "https://careloop.io/en/agb-en/",
+        "https://www.xayn.com/terms-of-use",
+        "https://www.teamviewer.com/eula/",
+    ]
+
+    feeds = [
+        "https://merics.org/en/rss",
+        "https://www.brot-fuer-die-welt.de/presse/",
+        "https://www.baumev.de/Presse.html",
+        "https://sanitygroup.com/press/",
+        "https://www.pro-bahn-berlin.de/blog.html",
+        "https://de.squarespace.com/blog/category/makers",
+        "https://door2door.io/de/news/mediathek/",
+        "https://einhorn.my/press-page/",
+        "https://www.hcu-hamburg.de/presse/news/news/",
+        "https://www.koblenz.de/coronavirus/aktuelle-meldungen/",
+        "https://www.lateinamerikaverein.de/de/aktuelles/corona-news",
+        "https://www.nakos.de/aktuelles/nachrichten/",
+        "https://power-shift.de/feed/",
+        "https://www.osnabrueck.de/start/archiv/nachrichtenarchiv.html",
+        "https://www.ge.com/power/about/press-releases",
+    ]
+
+    misc = [
+        "https://www.muelheim-ruhr.de/cms/shared/sitemap.php",
+        "https://cenior.de/Impressum",
+        "https://medexo.com/service-bereich/kontakt/",
+        "https://www.milkthesun.com/en/jobs",
+        "https://www.gfk.com/contact",
+    ]
+
+    def test_that_privacy_policies_are_ignored():
+        for url in privacy_policies:
+            assert ExtractPipeline.is_url_blocklisted(url) == True
+
+    def test_that_terms_and_conditions_are_ignored():
+        for url in terms_and_conditions:
+            assert ExtractPipeline.is_url_blocklisted(url) == True
+
+    def test_that_feeds_are_ignored():
+        for url in feeds:
+            assert ExtractPipeline.is_url_blocklisted(url) == True
+
+    def test_that_misc_pages_are_ignored():
+        for url in misc:
+            assert ExtractPipeline.is_url_blocklisted(url) == True
