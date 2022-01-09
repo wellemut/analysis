f25d788ae89e4e1d5089803c0405a22bd1fd0fa9
Finn Woelm <public@finnwoelm.com>
2022-01-09 12:59:45 +0100

    Scrape: Collect failed requests and write to database
    When a request fails (e.g., request timeout), intercept that failure and
    write the error message to the database. This allows us to easily
    identify failed requests later. Failed requests have a status_code of
    999.

--- scrape/ScrapeSpider.py
+++ scrape/ScrapeSpider.py
@@ -12,7 +12,7 @@ class ScrapeSpider(scrapy.Spider):
     # See: https://hexfox.com/p/how-to-filter-out-duplicate-urls-from-scrapys-start-urls/
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url)
+            yield scrapy.Request(url, errback=self.on_error)
 
     # Extract all links on the page
     # Docs: https://docs.scrapy.org/en/latest/topics/link-extractors.html
@@ -50,13 +50,24 @@ class ScrapeSpider(scrapy.Spider):
         if status_code in [301, 302, 303, 307, 308]:
             redirect_url = headers.get("Location")
             if redirect_url:
-                yield response.follow(
-                    redirect_url, callback=self.parse, cb_kwargs=dict(depth=depth)
-                )
+                yield self.follow(response, redirect_url, depth=depth)
 
         # Otherwise, look in entire body for new links
         elif content is not None:
             for link in self.extract_links(response):
-                yield response.follow(
-                    link.url, callback=self.parse, cb_kwargs=dict(depth=depth + 1)
-                )
+                yield self.follow(response, link.url, depth=depth + 1)
+
+    # Follow a link
+    def follow(self, response, url, **kwargs):
+        return response.follow(
+            url, callback=self.parse, errback=self.on_error, cb_kwargs=kwargs
+        )
+
+    # Handle a scraping error
+    def on_error(self, failure):
+        yield {
+            "url": failure.request.url,
+            "depth": failure.request.cb_kwargs.get("depth", 0),
+            "status_code": 999,
+            "content": repr(failure),
+        }
