256a661b70888f218f1f1c01d1723c8a416605cd
Finn Woelm <public@finnwoelm.com>
2022-01-09 20:37:40 +0100

    Remove tldextract: No longer used
    We do not currently use tldextract because instead of focusing on TLDs
    for the Website, we now take the entire domain (including subdomain), so
    that we can more accurately scrape websites that are subdomains. For
    example, earth.columbia.edu should focus only on its subdomain and not
    collect any content related to the root domain columbia.edu.
    
    See 4e802aebe6fd97437ffdc08e5f0491ebe8e2c0d8 for more context.

--- models/Webpage.py
+++ models/Webpage.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.hybrid import hybrid_property
-import tldextract
 import models
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
-import os
 import pytest
 from sqlalchemy.orm import close_all_sessions
 from vcr import use_cassette
-import tldextract
 from models import BaseModel, Website, Webpage
 
 # Truncate all tables
@@ -12,22 +10,6 @@ def reset_database():
     with BaseModel.session.begin():
         for table in BaseModel.metadata.tables:
             BaseModel.session.execute("TRUNCATE TABLE {} CASCADE".format(table))
-
-
-# Make a request with TLD extract to cache the Public Suffix List (PLS)
-# See: https://github.com/john-kurkowski/tldextract#note-about-caching
-@pytest.fixture(autouse=True, scope="session")
-def cache_tld_extract():
-    with use_cassette(
-        os.path.join(
-            os.path.dirname(__file__),
-            "casettes",
-            "conftest",
-            "cache_tld_extract.yaml",
-        ),
-        record_mode="once",
-    ):
-        tldextract.extract("example.com")
 
 
 @pytest.fixture(scope="session")
