e1d6ec81540639c0d5f3a1b2eb8bc84bdede4bc2
Finn Woelm <public@finnwoelm.com>
2022-01-10 22:40:52 +0100

    run.py: Use argparse to accept one or more domains to analyze
    Instead of requiring user to hardcode the domain to analyze into the
    run.py file, the file now accepts one or more domains as
    whitespace-separated positional arguments.

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
-from pipelines import (
-    ScrapePipeline,
-    ExtractPipeline,
-    LangDetectPipeline,
-    KeywordPipeline,
-)
+import argparse
 
-for domain in ["17ziele.de"]:
-    ScrapePipeline.process(domain)
-    ExtractPipeline.process(domain)
-    LangDetectPipeline.process(domain)
-    KeywordPipeline.process(domain)
+program = argparse.ArgumentParser(
+    description="Run one or several domains through all pipelines",
+    epilog="Example: python ./run.py example.com test.com",
+)
+program.add_argument("domain", nargs="+", help="one or more domains to analyze")
+args = program.parse_args()
+
+for domain in args.domain:
+    # Load pipelines only when needed, otherwise run.py --help becomes very slow
+    pipelines = __import__("pipelines")
+
+    # Run domain through pipelines
+    pipelines.ScrapePipeline.process(domain)
+    pipelines.ExtractPipeline.process(domain)
+    pipelines.LangDetectPipeline.process(domain)
+    pipelines.KeywordPipeline.process(domain)
